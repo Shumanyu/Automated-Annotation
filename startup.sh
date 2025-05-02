875562699c0e1954527a1872a512c17f69a9f099
Shumanyu <95439892+Shumanyu@users.noreply.github.com>
2025-05-03 01:56:01 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 pip uninstall -y opencv-python
 pip install opencv-python-headless
+pip install --no-cache-dir --upgrade numpy
+pip install --no-cache-dir --upgrade opencv-python-headless
